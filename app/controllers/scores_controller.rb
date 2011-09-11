775912a8ef7880b62afa830a879175689d87c43c
Your Name <dw_henry@yahoo.com.au>
2011-09-11 09:23:21 +0100

    diaplay a breakdown of player scores

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,6 +1,8 @@
 class ScoresController < ApplicationController
+  before_filter :require_user
+
   def index
-    @users = User.all
+    @users = User.all.sort_by(&:points)
     @matches = Match.all
   end
 end
--- app/helpers/scores_helper.rb
+++ app/helpers/scores_helper.rb
@@ -1,6 +1,21 @@
 module ScoresHelper
   def points_for(match, user)
-    return '&nbsp;' unless match.result.try(:home_team)
-    match.points_for_pick(match.picks.first(:conditions => {:user_id => user.id}).try(:pick) || 0)
+    pick = match.picks.first(:conditions => {:user_id => user.id})
+    if match.result.try(:home_team)
+      "#{points_for_pick(match, pick)} <span class='small'>(#{selection_for(pick) || 'none'})</span>"
+    elsif pick && pick.pick != 0
+      "<span class='small'>(#{selection_for(pick)})</span>"
+    else
+      return '&nbsp;'
+    end
+  end
+
+  private
+  def selection_for(pick)
+    pick.try(:description_with, pick.try(:pick))
+  end
+
+  def points_for_pick(match, pick)
+    match.points_for_pick(pick.try(:pick) || 0)
   end
 end
