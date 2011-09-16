9fa10d5261ea6a41c753691e7b01a39b529bbe07
Your Name <dw_henry@yahoo.com.au>
2011-09-16 14:05:28 +0100

    modifier cheat

--- db/migrate/20110916122741_fix_picks.rb
+++ db/migrate/20110916122741_fix_picks.rb
@@ -26,7 +26,12 @@ class FixPicks < ActiveRecord::Migration
       pick.update_attribute(:pick, value)
     else
       picks = match.picks(:include => :user, :conditions => ['users.id != ?', user])
-      value = picks.map{|p| p.pick}.sum.to_f / match.picks.size
+      value = picks.map{|p| p.match.points_for_pick(p.pick)}.sum.to_f / picks.size
+      if match.results.diff > 0
+        value +=match.results.diff
+      else
+        value = match.results.diff - value
+      end
       pick.update_attribute(:pick, value.to_i)
     end
   end
