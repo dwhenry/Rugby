f63f1934f5439c73dc822e46794750da11c049d6
David Henry <dw_henry@yahoo.com.au>
2014-12-09 20:16:15 +0000

    Add some logic around player deletion. previously anyone could remove a player from a league.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,22 +2,36 @@ class PlayersController < ApplicationController
   before_filter :require_user
 
   def create
-    @player = Player.new(params.require(:player).permit(:user_id, :league_id, :password))
+    @player = Player.new(player_params)
     if @player.save
       redirect_to league_path(@player.league_id)
     else
       @leagues = League.where.not(name: 'All Users')
       @all_league = League.find_by(name: 'All Users')
-      render :template => "leagues/index"
+      render template: "leagues/index"
     end
   end
 
   def destroy
-    Player.find(params[:id]).delete
+    if current_user.admin?
+      Player.find_by(id: params[:id])
+    else
+      Player.find_by(id: params[:id], user_id: current_user.id)
+    end.try(:delete)
+
     redirect_to main_index_path
   end
 
   def new
-    @player = Player.new(params.require(:player).permit(:user_id, :league_id, :league_password))
+    @player = Player.new(player_params)
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(
+      :league_id,
+      :password
+    ).merge(user_id: current_user.id)
   end
 end
